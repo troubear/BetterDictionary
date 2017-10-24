94057fdcd54d52c422eea7ce4e0cb491fad654bb
komatus <aligned32@gmail.com>
2017-10-25 00:28:32 +0900

    Optimized the string key.
    - Implemented the StringKey type that precalculate the hash.

--- Assets/Plugins/BetterDictionary/HashSet.cs
+++ Assets/Plugins/BetterDictionary/HashSet.cs
@@ -1,71 +1,70 @@
 ﻿using System.Collections.Generic;
-using UnsafeGeneric;
 
 #if !BETTER_PATCH
 namespace Better
 {
 #endif
-/// <summary>
-///     A generic hash set class optimized for the Unity.
-/// </summary>
-/// <typeparam name="T">The type of elements in the hash set.</typeparam>
-public class HashSet<T> : System.Collections.Generic.HashSet<T>
-{
-    private static readonly IEqualityComparer<T> EqualityComparer = EqualityComparerFactory.Create<T>();
-
     /// <summary>
-    ///     Initializes a new instance of the <see cref="T:Better.HashSet`1" /> class that is empty
-    ///     and uses the default equality comparer for the set type.
+    ///     A generic hash set class optimized for the Unity.
     /// </summary>
-    public HashSet() : this((IEqualityComparer<T>) null)
+    /// <typeparam name="T">The type of elements in the hash set.</typeparam>
+    public class HashSet<T> : System.Collections.Generic.HashSet<T>
     {
-    }
+        private static readonly IEqualityComparer<T> EqualityComparer = EqualityComparerFactory.Create<T>();
 
-    /// <summary>
-    ///     Initializes a new instance of the <see cref="T:Better.HashSet`1" /> class that is empty
-    ///     and uses the specified equality comparer for the set type.
-    /// </summary>
-    /// <param name="comparer">
-    ///     The <see cref="T:System.Collections.Generic.IEqualityComparer`1" /> implementation to use when
-    ///     comparing values in the set, or null to use the default
-    ///     <see cref="T:System.Collections.Generic.EqualityComparer`1" /> implementation for the set type.
-    /// </param>
-    public HashSet(IEqualityComparer<T> comparer) : base(comparer ?? EqualityComparer)
-    {
-    }
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="T:Better.HashSet`1" /> class that is empty
+        ///     and uses the default equality comparer for the set type.
+        /// </summary>
+        public HashSet() : this((IEqualityComparer<T>)null)
+        {
+        }
 
-    /// <summary>
-    ///     Initializes a new instance of the <see cref="T:Better.HashSet`1" /> class that uses the
-    ///     default equality comparer for the set type, contains elements copied from the specified collection, and has
-    ///     sufficient capacity to accommodate the number of elements copied.
-    /// </summary>
-    /// <param name="collection">The collection whose elements are copied to the new set.</param>
-    /// <exception cref="T:System.ArgumentNullException">
-    ///     <paramref name="collection" /> is null.
-    /// </exception>
-    public HashSet(IEnumerable<T> collection) : this(collection, null)
-    {
-    }
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="T:Better.HashSet`1" /> class that is empty
+        ///     and uses the specified equality comparer for the set type.
+        /// </summary>
+        /// <param name="comparer">
+        ///     The <see cref="T:System.Collections.Generic.IEqualityComparer`1" /> implementation to use when
+        ///     comparing values in the set, or null to use the default
+        ///     <see cref="T:System.Collections.Generic.EqualityComparer`1" /> implementation for the set type.
+        /// </param>
+        public HashSet(IEqualityComparer<T> comparer) : base(comparer ?? EqualityComparer)
+        {
+        }
 
-    /// <summary>
-    ///     Initializes a new instance of the <see cref="T:Better.HashSet`1" /> class that uses the
-    ///     specified equality comparer for the set type, contains elements copied from the specified collection, and has
-    ///     sufficient capacity to accommodate the number of elements copied.
-    /// </summary>
-    /// <param name="collection">The collection whose elements are copied to the new set.</param>
-    /// <param name="comparer">
-    ///     The <see cref="T:System.Collections.Generic.IEqualityComparer`1" /> implementation to use when
-    ///     comparing values in the set, or null to use the default
-    ///     <see cref="T:System.Collections.Generic.EqualityComparer`1" /> implementation for the set type.
-    /// </param>
-    /// <exception cref="T:System.ArgumentNullException">
-    ///     <paramref name="collection" /> is null.
-    /// </exception>
-    public HashSet(IEnumerable<T> collection, IEqualityComparer<T> comparer)
-        : base(collection, comparer ?? EqualityComparer)
-    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="T:Better.HashSet`1" /> class that uses the
+        ///     default equality comparer for the set type, contains elements copied from the specified collection, and has
+        ///     sufficient capacity to accommodate the number of elements copied.
+        /// </summary>
+        /// <param name="collection">The collection whose elements are copied to the new set.</param>
+        /// <exception cref="T:System.ArgumentNullException">
+        ///     <paramref name="collection" /> is null.
+        /// </exception>
+        public HashSet(IEnumerable<T> collection) : this(collection, null)
+        {
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="T:Better.HashSet`1" /> class that uses the
+        ///     specified equality comparer for the set type, contains elements copied from the specified collection, and has
+        ///     sufficient capacity to accommodate the number of elements copied.
+        /// </summary>
+        /// <param name="collection">The collection whose elements are copied to the new set.</param>
+        /// <param name="comparer">
+        ///     The <see cref="T:System.Collections.Generic.IEqualityComparer`1" /> implementation to use when
+        ///     comparing values in the set, or null to use the default
+        ///     <see cref="T:System.Collections.Generic.EqualityComparer`1" /> implementation for the set type.
+        /// </param>
+        /// <exception cref="T:System.ArgumentNullException">
+        ///     <paramref name="collection" /> is null.
+        /// </exception>
+        public HashSet(IEnumerable<T> collection, IEqualityComparer<T> comparer)
+            : base(collection, comparer ?? EqualityComparer)
+        {
+        }
     }
-}
 #if !BETTER_PATCH
 }
 #endif
---
+++ Assets/Plugins/BetterDictionary/StringKey.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using UnsafeGeneric;
+
+#if !BETTER_PATCH
+namespace Better
+{
+#endif
+    /// <summary>
+    ///     A string key class optimized for the Dictionary and HashSet.
+    /// </summary>
+    public struct StringKey : IEquatable<StringKey>
+    {
+        /// <summary>
+        ///     The string of the key.
+        /// </summary>
+        public readonly string Value;
+
+        /// <summary>
+        ///     The length of the string.
+        /// </summary>
+        public readonly int Length;
+
+        /// <summary>
+        ///     The hash code of the string.
+        /// </summary>
+        public readonly int HashCode;
+
+        public StringKey(string str)
+        {
+            Value = str;
+            Length = str.Length;
+            HashCode = StringHashCode.Calculate(str, Length);
+        }
+
+        public bool Equals(StringKey key)
+        {
+            return Length == key.Length && Value.Equals(key.Value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is StringKey)
+            {
+                return Equals((StringKey)obj);
+            }
+
+            // exception will be thrown later for null this
+            return this == null;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode;
+        }
+
+        public static bool operator ==(StringKey a, StringKey b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(StringKey a, StringKey b)
+        {
+            return !a.Equals(b);
+        }
+
+        // StringKey key = "Foo";
+        public static implicit operator StringKey(string str)
+        {
+            return new StringKey(str);
+        }
+
+        // StringKey key;
+        // string str = key;
+        public static implicit operator string(StringKey str)
+        {
+            return str.Value;
+        }
+    }
+#if !BETTER_PATCH
+}
+#endif
